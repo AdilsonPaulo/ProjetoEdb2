008e1c29dc5e763e42b67c50d238039bc8c79aa8
AdilsonPaulo <112577417+AdilsonPaulo@users.noreply.github.com>
2023-05-21 17:37:03 -0300

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+        BinaryTreeSearch tree = new BinaryTreeSearch();
+        
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+        
+        tree.delete(40);
+        
+        System.out.println("Enesimo Elemento: " + tree.enesimoElemento(3));
+        System.out.println("Posição do Elemento: " + tree.posicao(60));
+        
+	}
+}
