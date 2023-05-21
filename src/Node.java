ccfab268b789e65467bba632bd3e02a94c876134
AdilsonPaulo <112577417+AdilsonPaulo@users.noreply.github.com>
2023-05-21 17:38:24 -0300

    Create Node.java

---
+++ src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    
+    int key;
+    Node left, right;
+
+    public Node(int item) {
+        this.key = item;
+        this.left = null;
+        this.right = null;
+    }
+}
